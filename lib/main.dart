83557f705e7810f600db59479d80eddfccabcc92
Otaku288 <rtishevandrey7777@gmail.com>
2023-10-17 22:37:03 +0300

    classwork

---
+++ lib/main.dart
@@ -0,0 +1,140 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: ClassWork()));
+}
+
+class ClassWork extends StatelessWidget {
+  const ClassWork({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+      title: Text('Задание'),
+      centerTitle: true,
+    ),
+    
+    body: Flex(
+      direction: Axis.vertical,
+      children: [
+        Flexible(
+          flex: 1,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Flexible(
+                flex: 1,
+                child: Container(
+                  margin: EdgeInsets.all(3),
+                  width: 100,
+                  height: 220,
+                  color: Colors.green,
+                )
+              ),
+
+              Flexible(
+                flex: 1,
+                child: Container(
+                  margin: EdgeInsets.all(3),
+                  width: 100,
+                  height: 220,
+                  color: Colors.green,
+                )
+              )],
+          )
+        ),
+
+        Flexible(
+          flex: 1,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Flexible(
+                flex: 1,
+                child: Container(
+                  margin: EdgeInsets.all(3),
+                  width: 100,
+                  height: 80,
+                  color: Colors.amber,
+                ),
+              ),
+              Flexible(
+                flex: 1,
+                child: Container(
+                  margin: EdgeInsets.all(3),
+                  width: 100,
+                  height: 80,
+                  color: Colors.amber,
+
+                )
+              )
+            ],
+          ),
+
+            
+        ),
+
+       
+        Flexible(
+          flex: 1,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Flexible(
+                flex: 1,
+                child: Container(
+                  margin: EdgeInsets.all(3),
+                  width: 100,
+                  height: 80,
+                  color: Colors.amber,
+                )
+              ),
+
+              Flexible(
+                flex: 1,
+                child: Container(
+                  margin: EdgeInsets.all(3),
+                  width: 100,
+                  height: 80,
+                  color: Colors.amber,
+                )
+              )
+            ],
+          )
+        ),
+
+        Flexible(
+          flex: 1,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Flexible(
+                flex: 1,
+                child: Container(
+                  margin: EdgeInsets.all(3),
+                  width: 100,
+                  height: 220,
+                  color: Colors.green,
+                )
+              ),
+              Flexible(
+                flex: 1,
+                child: Container(
+                  margin: EdgeInsets.all(3),
+                  width: 100,
+                  height: 220,
+                  color: Colors.green,
+                )
+              )
+            ],
+          )
+        )
+        
+      ],
+    )
+
+    );
+  }
+
+}
